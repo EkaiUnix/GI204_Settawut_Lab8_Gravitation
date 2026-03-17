b496d67a58ddbcaecbe3a7034bc3a463f06a01d4
EkaiUnix <settawut.pakd@bumail.net>
2026-03-17 16:23:36 +0700

    Clone

---
+++ Assets/Planets of the Solar System 3D/Gravitational.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Gravitational : MonoBehaviour
+{
+    public static List<Gravitational> otherGameObject;
+    private Rigidbody rb;
+    const float G = 0.006674f;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (otherGameObject == null) 
+        {
+            otherGameObject = new List<Gravitational>();
+        }
+        otherGameObject.Add(this);
+    }
+
+    void FixedUpdate()
+    {
+        foreach (Gravitational obj in otherGameObject) 
+        {
+            if (obj != this) 
+            {
+                AttractionForce(obj);
+            }
+        }
+    }
+
+    void AttractionForce(Gravitational other) 
+    {
+        Rigidbody otherRb = other.rb;
+        Vector3 dir = rb.position - otherRb.position;
+        float dist = dir.magnitude;
+        if (dist == 0f) 
+        {
+            return;
+        }
+        float forceMagnitude = G * ((rb.mass * otherRb.mass) / Mathf.Pow(dist, 2));
+        Vector3 gravitationalForce = forceMagnitude * dir.normalized;
+        otherRb.AddForce(gravitationalForce);
+    }
+}
